aa79e31e009c062dc248fb79107a42ce2d33d21d
yashbim <ybimsara03@gmail.com>
2025-01-14 23:55:36 +0530

    Created variables in BankAccount.java

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,4 +1,9 @@
 package org.example;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
+    private int accountID;
+    private double balance;
+    private ReentrantLock reentrantLock;
 }
