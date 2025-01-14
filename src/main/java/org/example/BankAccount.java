9e0d8e0746197cd31a9d84765844b5827c3783b2
yashbim <ybimsara03@gmail.com>
2025-01-15 00:10:05 +0530

    Constructor created

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -4,6 +4,14 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class BankAccount {
     private int accountID;
-    private double balance;
+    private double accountBalance;
     private ReentrantLock reentrantLock;
+
+    public BankAccount(int accountID, double balance) {
+        this.accountID = accountID;
+        this.accountBalance = balance;
+        this.reentrantLock = new ReentrantLock();
+    }
+
 }
+
