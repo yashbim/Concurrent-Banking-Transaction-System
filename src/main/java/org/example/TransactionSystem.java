510a79482432945e8fb4b356540eb67503911246
yashbim <ybimsara03@gmail.com>
2025-01-16 03:40:35 +0530

    TransactionSystem Constructor Code added in ### TransactionSystem.java

--- src/main/java/org/example/TransactionSystem.java
+++ src/main/java/org/example/TransactionSystem.java
@@ -1,9 +1,17 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class TransactionSystem {
     private Map<Integer, BankAccount> accounts;
 
+    public TransactionSystem(List<BankAccount> accountList){
+        accounts = new HashMap<>();
+        for(BankAccount account: accountList){
+        }
+    }
 
 }
