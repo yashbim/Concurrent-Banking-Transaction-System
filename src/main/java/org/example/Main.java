7856d3af8506cbd6857c69d6fc0e751fae1c15e7
yashbim <ybimsara03@gmail.com>
2025-01-16 04:36:05 +0530

    Created sample bank accounts

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,19 @@
 package org.example;
 // Find source code at https://github.com/yashbim/Concurrent-Banking-Transaction-System
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        //creating bank accounts
+
+        List<BankAccount> bankAccounts = Arrays.asList();
+            new BankAccount(1, 100000);
+            new BankAccount(2, 50000);
+            new BankAccount(3, 25000);
     }
 }
