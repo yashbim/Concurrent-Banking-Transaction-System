50432519067d367f1b16e77fdbd07fa08f086f59
yashbim <ybimsara03@gmail.com>
2025-01-16 04:27:09 +0530

    Created constructor in thread class

--- src/main/java/org/example/TransactionThread.java
+++ src/main/java/org/example/TransactionThread.java
@@ -1,4 +1,18 @@
 package org.example;
 
-public class TransactionThread {
+public class TransactionThread implements Runnable{
+    private TransactionSystem transactionSystem;
+    private int account1_ID;
+    private int account2_ID;
+    private double amount;
+
+    public TransactionThread(TransactionSystem transactionSystem, int account1_ID, int account2_ID, double amount){
+        this.transactionSystem = transactionSystem;
+        this.account1_ID = account1_ID;
+        this.account2_ID = account2_ID;
+        this.amount = amount;
+    }
+
+    @Override
+    public void run() {}
 }
