6729f18eea6a552d84b7b41ba49ce1e354998c4a
yashbim <ybimsara03@gmail.com>
2025-01-16 04:07:24 +0530

    Locks added

--- src/main/java/org/example/TransactionSystem.java
+++ src/main/java/org/example/TransactionSystem.java
@@ -8,11 +8,24 @@ import java.util.Map;
 public class TransactionSystem {
     private Map<Integer, BankAccount> accounts;
 
+    //constructor
     public TransactionSystem(List<BankAccount> accountList){
         accounts = new HashMap<>();
         for(BankAccount account: accountList){
             accounts.put(account.getAccountID(), account);
         }
+    }
+
+    //account 1 will transfer money to account 2
+    public boolean transferMoney(int  account1_ID, int account2_ID, double amount){
+        BankAccount account1 = accounts.get(account1_ID);
+        BankAccount account2 = accounts.get(account2_ID);
+
+        // Locking Accounts based on order
+        BankAccount lock1 = account1_ID < account2_ID ? account1 : account2;
+        BankAccount lock2 = account1_ID < account2_ID ? account2 : account1;
+
+        return true;
     }
 
 }
